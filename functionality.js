b4f64f76c9a69adaddb8c505925910e804ca3311
GushteraDimityr <110856209+4ushkopeka@users.noreply.github.com>
2023-09-08 08:02:00 +0300

    Fiddled with FAQ

--- functionality.js
+++ functionality.js
@@ -28,4 +28,21 @@ function load(){
         }
         prevScrollpos = currentScrollPos;
       }   
+      let faq = document.querySelector('body > header > nav > ul > a.highlighted');
+      if(faq.textContent === 'FAQ'){
+        window.onclick = () => setTimeout(footerFunc, 355);
+
+    }
+
+    function footerFunc(){
+        let openedAcc = document.getElementsByClassName('show');
+        let footer = document.getElementsByTagName('footer')[0];
+        console.log(openedAcc.length);
+        if(openedAcc.length>=1){
+          footer.style.transform='translateY(15vh)';
+        }
+        else{
+          footer.style.transform='translateY(0vh)';
+        }
+      }
 }
