c4c79ce2d3ae57f014ad1b367e0df40f2329113b
GushteraDimityr <110856209+4ushkopeka@users.noreply.github.com>
2023-09-13 04:30:21 +0300

    Full Dashboard functionality

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -2,18 +2,20 @@ function grading(){
     load();
     let objArr = [];
     let barUpdates = [];
-    let q1, q2, q3, q4, more = {};
-    let blocks = [q1, q2, q3, q4, more]
+    let quartiles = [];
+    objectMaker(quartiles);
+    let totalExam = 0;
+    let totalCredits = 0;
     tableManipulation();
 
     function tableManipulation(){
       let exams = document.querySelectorAll('.exam');
+      totalExam = exams.length; //!!!
       for (const exam of exams) {
         let obj = {};
-        obj.exam = exam;
-        obj.ec = null;
-        obj.grade = null;
-        obj.block = null;
+        obj.exam = exam;  //cells
+        obj.ec = null;  //cells
+        obj.grade = null;  //cells
         let children = exam.parentElement.children.length;
         if(children<=3){
             obj.ec = exam.parentElement.children[1];
@@ -23,9 +25,12 @@ function grading(){
             obj.ec = exam.parentElement.children[3];
             obj.grade = exam.parentElement.children[4];
         }
+        obj.quartile = quartileManipulation(obj);
         obj.exam.addEventListener('click', tdHandler, true);
         objArr.push(obj);
+        totalCredits += Number(obj.ec.textContent);
       }
+      
     }
 
     async function tdHandler(e){
@@ -37,30 +42,151 @@ function grading(){
                 break;
             }
         }
-        if(grade!=null){
+        if(grade!=null && grade>0 && grade<=10){
             currentObj.exam.removeEventListener('click', tdHandler, true);
             currentObj.grade.textContent = Number(grade);
+
+            quartiles[currentObj.quartile-1].grades.push(Number(grade));    
             currentObj.exam.style.pointerEvents = "none";
             await new Promise(r => setTimeout(r, 1000));
             currentObj.exam.classList.remove('exam');
             if(grade<5.5){
-                currentObj.exam.style.backgroundColor = "rgba(253, 29, 29, 0.2)";
+                currentObj.exam.style.backgroundColor = "rgba(253, 29, 29, 0.25)";
+                quartiles[currentObj.quartile-1].examsFailed++;
             }
             else{
-                currentObj.exam.style.backgroundColor = "rgba(143, 253, 29, 0.2)";
-                let bar = document.getElementsByClassName('progress')[0];
+                currentObj.exam.style.backgroundColor = "rgb(51 255 0 / 25%)";
+
                 let width = Number(currentObj.ec.textContent);
+                currentObj.credits = Number(currentObj.ec.textContent);
+                quartiles[currentObj.quartile-1].examsTaken++;
+                quartiles[currentObj.quartile-1].creditsTaken+=Number(currentObj.ec.textContent);
+                
+                let bar = document.getElementsByClassName('progress')[0];
                 barUpdates.push(width);
-                bar.style.width = (Sum()*10/6).toString()+'%';
+                bar.style.width = (arrayHandler(barUpdates, 'sum')*10/6).toString()+'%';
             }
+            overviewHandler(currentObj);
+        }
+    }
+    
+    function quartileManipulation(obj){
+        let q = 0;
+        if(Array.from(obj.exam.classList).includes('q1')){
+            q = 1;
+            quartiles[0].maxCredits+=Number(obj.ec.textContent);
+            quartiles[0].maxTaken+=1;   
+        }
+        else if(Array.from(obj.exam.classList).includes('q2')){
+            q = 2;
+            quartiles[1].maxCredits+=Number(obj.ec.textContent);
+            quartiles[1].maxTaken+=1;
+        }
+        else if(Array.from(obj.exam.classList).includes('q3')){
+            q = 3;
+            quartiles[2].maxCredits+=Number(obj.ec.textContent);
+            quartiles[2].maxTaken+=1;
+        }
+        else if(Array.from(obj.exam.classList).includes('q4')){
+            q = 4;
+            quartiles[3].maxCredits+=Number(obj.ec.textContent);
+            quartiles[3].maxTaken+=1;
+        }
+        else {
+            q = 5;
+            quartiles[4].maxCredits+=Number(obj.ec.textContent);
+            quartiles[4].maxTaken+=1;
+        }
+        return q;
+    }
+
+    function overviewHandler(exam){
+        let summaryCells = getOverviewQuartileCells(exam.quartile);
+        let overallValues = overallHandler();
+        let index = exam.quartile-1;
+        summaryCells[0].textContent = `${quartiles[index].examsTaken}/${quartiles[index].maxTaken}`;
+        summaryCells[1].textContent = `${quartiles[index].creditsTaken}/${quartiles[index].maxCredits}`;
+        summaryCells[2].textContent = `${arrayHandler(quartiles[index].grades, 'avg')}`;
+        document.querySelector('#overall > td:nth-child(2)').textContent = `${overallValues[0]}/${totalExam}`;
+        document.querySelector('#overall > td:nth-child(3)').textContent = `${overallValues[1]}/${totalCredits}`;
+        document.querySelector('#overall > td:nth-child(4)').textContent = `${overallValues[2]}`;
+    }
+
+    function getOverviewQuartileCells(quartile){
+        let examTakenCell = null;
+        let ecTakenCell = null;
+        let avgGradeCell = null;
+        if(quartile === 1){
+            examTakenCell = document.querySelector('#overview > tbody > tr:nth-child(1) > td:nth-child(2)');
+            ecTakenCell = document.querySelector('#overview > tbody > tr:nth-child(1) > td:nth-child(3)');
+            avgGradeCell = document.querySelector('#overview > tbody > tr:nth-child(1) > td:nth-child(4)');
+        }
+        else if(quartile === 2){
+            examTakenCell = document.querySelector('#overview > tbody > tr:nth-child(2) > td:nth-child(2)');
+            ecTakenCell = document.querySelector('#overview > tbody > tr:nth-child(2) > td:nth-child(3)');
+            avgGradeCell = document.querySelector('#overview > tbody > tr:nth-child(2) > td:nth-child(4)');
+        }
+        else if(quartile === 3){
+            examTakenCell = document.querySelector('#overview > tbody > tr:nth-child(3) > td:nth-child(2)');
+            ecTakenCell = document.querySelector('#overview > tbody > tr:nth-child(3) > td:nth-child(3)');
+            avgGradeCell = document.querySelector('#overview > tbody > tr:nth-child(3) > td:nth-child(4)');
+        }
+        else if(quartile === 4){
+            examTakenCell = document.querySelector('#overview > tbody > tr:nth-child(4) > td:nth-child(2)');
+            ecTakenCell = document.querySelector('#overview > tbody > tr:nth-child(4) > td:nth-child(3)');
+            avgGradeCell = document.querySelector('#overview > tbody > tr:nth-child(4) > td:nth-child(4)');
+        }
+        else{
+            examTakenCell = document.querySelector('#overview > tbody > tr:nth-child(5) > td:nth-child(2)');
+            ecTakenCell = document.querySelector('#overview > tbody > tr:nth-child(5) > td:nth-child(3)');
+            avgGradeCell = document.querySelector('#overview > tbody > tr:nth-child(5) > td:nth-child(4)');
+        }
+        return [examTakenCell, ecTakenCell, avgGradeCell];
+    }
+
+    function arrayHandler(arr, key){
+        
+        if(key === 'sum'){
+            let sum = 0;
+            for (const num of arr) {
+                sum+=num;
+            }
+            return sum;
+        }
+        else if(key === 'avg'){
+            let avg = 0;
+            for (const num of arr) {
+                avg+=num;
+            }
+            return (avg/arr.length).toFixed(1);
         }
     }
 
-    function Sum(){
-        let sum = 0;
-        for (const num of barUpdates) {
-            sum+=num;
+    function objectMaker(arr){
+        for (let index = 0; index < 5; index++) {
+            let quartile = {
+                examsTaken:0,
+                examsFailed:0,
+                creditsTaken:0,
+                grades: [],
+                maxCredits:0,
+                maxTaken:0
+            };
+            arr.push(quartile);
         }
-        return sum;
+    }
+
+    function overallHandler(){
+        let takenExams = 0;
+        let takenCredits = 0;
+        let totalExams = 0;
+        let avgGrade = 0;
+        for (const obj of quartiles) {
+            totalExams+=(obj.examsTaken + obj.examsFailed);
+            takenExams+=obj.examsTaken;
+            takenCredits += obj.creditsTaken;
+            avgGrade+=arrayHandler(obj.grades, 'sum');
+        }
+        return [takenExams, takenCredits, (avgGrade/totalExams).toFixed(1)];
     }
 }
